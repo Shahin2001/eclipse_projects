9888b3640524a50a241df9aab06628866be2535b
ADMIN <ADMIN@DESKTOP-3JGAGNP>
2024-09-03 16:25:54 +0530

    commit

---
+++ max/src/max/maximum.java
@@ -0,0 +1,15 @@
+package max;
+
+public class maximum {
+	public static void main(String args[]) {
+		int a=30, b=60;
+		if(a>b) {
+			System.out.println(a+"is maximum");		
+			}
+		else {
+			System.out.println(b+"is maximum");	
+		}
+		
+	}
+
+}
