4595e6bae5cc8028e3e9c091a721d898f4b258f8
ADMIN <ADMIN@DESKTOP-3JGAGNP>
2024-09-03 16:31:07 +0530

    commit

---
+++ Prac/src/com/Prime.java
@@ -0,0 +1,102 @@
+package com;
+
+public class Prime {
+	public static void main(String[] args) {
+//        prime
+//		int n=4, count=0;
+//		for(int i=1;i<=n;i++) {
+//			if(n%i==0) {
+//				count++;
+//			}
+//			
+//		}
+//		if(count==2) {
+//			System.out.println("prime");
+//		}
+//		else {
+//			System.out.println("Not prime");
+//		}
+		
+//		reverse no
+//		int n=123,rem,rev=0;
+//		while(n>0) {
+//			rem=n%10;
+//			rev=rev*10+rem;
+//			n=n/10;
+//		}
+//		System.out.println(rev);
+		
+		
+		
+//		palindrome
+//		int n=121,rem,rev=0,temp=n;
+//		while(n>0) {
+//			rem=n%10;
+//			rev=rev*10+rem;
+//			n=n/10;
+//			
+//		}
+//		if(temp==rev) {
+//			System.out.println("palindrome");
+//		}
+//		else {
+//			System.out.println("not palindrome");
+//		}
+		
+		
+		
+		
+//		factorial
+//		int n=5,fact=1;
+//		for(int i=1;i<=n;i++) {
+//			fact=fact*i;
+//		}
+//		System.out.println(fact);
+		
+		
+		
+//		sum
+//		int n=5,sum=0;
+//		for(int i=1;i<=n;i++) {
+//			sum=sum+i;
+//		}
+//		System.out.println(sum);
+		
+		
+		
+//		count
+//		int n=123, count=0;
+//		for(int i=n;i>0;i/=10) {
+//			count++;
+//		}
+//		System.out.println(count);
+		
+		
+//		armstrong
+//		int n=121, count=0,rem,sum=0;
+//		for(int i=n;i>0;i/=10) {
+//			count++;
+//		}
+//		System.out.println(count);
+//		
+//		for(int i=n;i>0;i/=10) {
+//			rem=i%10;
+//			
+//			 sum+= Math.pow(rem, count);
+//			//sum=sum+res;
+//		}
+//		System.out.println(sum);
+//		if(n==sum) {
+//			System.out.println("arm");
+//		}
+//		else {
+//			System.out.println("not arm");
+//		}
+		
+		
+		
+		
+		
+	}
+
+}
